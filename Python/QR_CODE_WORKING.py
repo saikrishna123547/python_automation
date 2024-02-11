8f08215807b6f5852a4ebe84c9f7c66374706921
saikrishna123547 <68315030+saikrishna123547@users.noreply.github.com>
2024-02-11 21:36:04 +0530

    Update QR_CODE_WORKING.py

--- Python/QR_CODE_WORKING.py
+++ Python/QR_CODE_WORKING.py
@@ -1,14 +1,17 @@
+# first run below commands on local machine to install the modules
 #pip install pyqrcode
+#pip install pypng
 
 import pyqrcode 
 from pyqrcode import QRCode 
+import png
   
 # String which represent the QR code 
-s = "https://www.youtube.com/channel/UCBz4yaxNxfiz1XYh-07UfWQ"
+# s = "https://www.youtube.com/channel/UCBz4yaxNxfiz1XYh-07UfWQ"
+s="www.geeksforgeeks.org"
   
 # Generate QR code 
 url = pyqrcode.create(s) 
   
 # Create and save the png file naming "myqr.png" 
-#Scalable Vector Graphics
-url.svg("myyoutube.svg", scale = 8) 
+url.png('myqr.png', scale = 6) 
