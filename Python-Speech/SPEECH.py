de727b9c5a6cea0d9fe1f128a672b68012b6e920
saikrishna123547 <68315030+saikrishna123547@users.noreply.github.com>
2024-02-11 19:19:56 +0530

    Update SPEECH.py
    some of the functions are deprecated hence I updated them.

--- Python-Speech/SPEECH.py
+++ Python-Speech/SPEECH.py
@@ -1,11 +1,9 @@
-#yPDF2 is a free and open-source pure-python 
-# pip install PyPDF2
+#PyPDF2 is a free and open-source pure-python 
 # PDF library capable of splitting, merging, cropping, and transforming the pages of PDF files
-
-
 #pyttsx3 is a text-to-speech conversion library in Python
 
-
+#run below two commands in local machine for the first time to download the packages.
+#pip install PyPDF2
 #pip install pyttsx3
 
 
@@ -13,15 +11,15 @@ import PyPDF2
 
 import pyttsx3
 
-pdfReader = PyPDF2.PdfFileReader(open('dummypdf.pdf', 'rb'))
+pdfReader = PyPDF2.PdfReader(open('dummypdf.pdf', 'rb'))
 
 #######CASE 1 PDF READ
 # Initialize the Pyttsx3 engine
 
 speaker = pyttsx3.init()
 
-for page_num in range(pdfReader.numPages):
-    text =  pdfReader.getPage(page_num).extractText()
+for page_num in range(len(pdfReader.pages)):
+    text =  pdfReader.pages[page_num].extract_text()
     speaker.say(text)
     speaker.runAndWait()
 speaker.stop()
